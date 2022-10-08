ea0ba45543385703af6d539dd92b364d79cba628
Artyom Shirshov <artyomsh01@yandex.ru>
2022-10-08 18:55:45 +0300

    Cards base logic done

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,7 +3,7 @@ package models
 type Card struct {
 	ID int `json:"id,omitempty" db:"id"`
 	Name string `json:"name,omitempty" db:"name"`
-	Done bool `json:"done,omitempty" db:"done"`
+	Done *bool `json:"done,omitempty" db:"done"`
 	ImgUrl string `json:"imgUrl,omitempty" db:"imgurl"`
 	StartTime string `json:"startTime,omitempty" db:"starttime"`
 	EndTime string `json:"endTime,omitempty" db:"endtime"`
